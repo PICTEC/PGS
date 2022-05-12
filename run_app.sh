d6cb6ddb9de298725e002c8166f7239f88b2f6bd
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-05-12 10:45:25 +0200

    restore migrations

--- run_app.sh
+++ run_app.sh
@@ -13,6 +13,25 @@ done
 >&2 echo "Migrate"
 nohup python manage.py migrate
 
+>&2 echo "Create superuser"
+nohup python manage.py create_init_user $DJANGO_SUPERUSER_USERNAME $DJANGO_SUPERUSER_EMAIL $DJANGO_SUPERUSER_PASSWORD
+
+
+>&2 echo "Importing parking areas"
+nohup python manage.py import_geojson_parking_areas $PARKING_AREAS_PATH $PARKING_AREAS_ADDRESS
+>&2 echo "Importing payment zones"
+nohup python manage.py import_geojson_payment_zones $PAYMENT_ZONES_PATH $PAYMENT_ZONES_ADDRESS
+>&2 echo "Import regions"
+nohup python manage.py import_geojson_regions $REGIONS_PATH $REGIONS_ADDRESS
+>&2 echo "Import parking terminals"
+if [ "$TERMINALS_LOCAL" = TRUE ] ; then
+  nohup python manage.py import_parking_terminals $TERMINALS_ADDRESS --local
+else
+  nohup python manage.py import_parking_terminals $TERMINALS_ADDRESS
+fi
+>&2 echo "Fill parking regions"
+nohup python manage.py fill_parking_regions
+
 if [ "$CONSOLE_EMAIL" = TRUE ] ; then
   >&2 echo "Starting SMTP debugging server"
   export PYTHONUNBUFFERED=1
